d484b84e3a332d05b07344f0f4e15b6ed30f2f16
Egor Duvalov <happytosiu@gmail.com>
2021-02-22 13:42:13 +0300

    UserDAO is Singleton now

--- src/main/java/com/innowise/duvalov/dao/UserDAO.java
+++ src/main/java/com/innowise/duvalov/dao/UserDAO.java
@@ -5,11 +5,16 @@ import com.innowise.duvalov.pool.ConnectionPool;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class UserDAO {
+public enum UserDAO {
+    INSTANCE;
     private static final String WRITE_USER_TO_DB =
-            "INSERT INTO USERS (login, password, role) VALUES (?,?,?)";
+            "INSERT INTO USERS (login, password, role,email) VALUES (?,?,?,?)";
+
+    private static final String TAKE_USER_BY_LOGIN =
+            "SELECT COUNT(id) FROM users WHERE login = ?";
 
     public void writeToDB(User user) {
         Connection connection = ConnectionPool.INSTANCE.getConnection();
@@ -17,10 +22,29 @@ public class UserDAO {
             ps.setString(1, user.getLogin());
             ps.setString(2, user.getPassword());
             ps.setInt(3, user.getRole().getRoleNumber());
+            ps.setString(4, user.getEmail());
             ps.executeUpdate();
             connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Returns amount of users with provided login
+     */
+    public int findUserByLogin(String login) {
+        ConnectionPool.INSTANCE.openPool();
+        try (Connection connection = ConnectionPool.INSTANCE.getConnection();
+             PreparedStatement ps = connection.prepareStatement(TAKE_USER_BY_LOGIN)
+        ) {
+            ps.setString(1, login);
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            return rs.getInt(1);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return 1;
     }
 }
